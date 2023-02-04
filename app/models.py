81a754104b9c30ffae686d68ee67ff8e98bd5117
sanket agre <sanketagre2498@gmail.com>
2023-02-04 12:43:14 +0530

    user, category and product fields are created. initial commit

---
+++ app/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+import os
+# Create your models here.
+
+def get_file_path(request, filename):
+    original_filename=filename
+    nowTime=datetime.datetime.now.strtime('%d%m%Y :%H:%M:%s')
+    filename="%s%s" % (nowTime.original_filename)
+    return os.path.join('upload/', filename)
+
+class Category(models.Model):
+    slug=models.CharField(max_length=150, null=False, blank=False)
+    cat_name=models.CharField(max_length=50, null=False, blank=False)
+    image=models.ImageField(upload_to=get_file_path, null=True, blank=True)
+    description=models.TextField(max_length=500, null=False, blank=False)
+    ststus=models.BooleanField(default=False,help_text="0=default, 1=Hidden")
+    trending=models.BooleanField(default=False,help_text="0=default, 1=Trending")
+    meta_titile=models.TextField(max_length=500, null=False, blank=False)
+    meta_desc=models.TextField(max_length=500, null=False, blank=False)
+    meta_keywords=models.TextField(max_length=500, null=False, blank=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cat_name
+
+class Product(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug=models.CharField(max_length=150, null=False, blank=False)
+    prod_name=models.CharField(max_length=150, null=False, blank=False)
+    image=models.ImageField(upload_to=get_file_path, null=True, blank=True)
+    description=models.TextField(max_length=500, null=False, blank=False)
+    small_desc=models.TextField(max_length=500, null=False, blank=False)
+    quantity=models.IntegerField(null=False, blank=False)
+    original_price=models.FloatField(null=False, blank=False)
+    selling_price=models.FloatField(null=False, blank=False)
+    ststus=models.BooleanField(default=False,help_text="0=default, 1=Hidden")
+    trending=models.BooleanField(default=False,help_text="0=default, 1=Trending")
+    tag=models.CharField(max_length=150, null=False, blank=False)
+    meta_titile=models.CharField(max_length=150, null=False, blank=False)
+    meta_desc=models.CharField(max_length=150, null=False, blank=False)
+    meta_keywords=models.CharField(max_length=150, null=False, blank=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.prod_name
+
+
---
+++ static/js/style.js
@@ -0,0 +1,7 @@
+function xyz(){
+    console.log("xyz")
+    document.getElementById("san").style.color = "red";
+}
+// document.getElementById("san").addEventListener('click',()=>{
+//     document.getElementById("san").style.color = "red";
+// })
