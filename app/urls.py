1837d21ffd2c08a451182a5b2e9028bc02be62e2
sanket agre <sanketagre2498@gmail.com>
2023-02-07 17:57:24 +0530

    commiting category and categoryviews page. and modifing model.py file

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,19 @@
 
-from django.contrib import admin
 from . import views
-from django.urls import path,include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path( '', views.index, name="home"),
-    path('category', views.category, name="category"),
+    path('collections', views.collections, name="collections"),
+    path('collections/<str:slug>',  views.collectionviews, name="collectionviews"),
 ]
+
+
+
+
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- app/views.py
+++ app/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     return render(request,'app/index.html')
 
-def category(request):
+def collections(request):
     category=Category.objects.filter(status=0)
-    contex={category:'category'}
+    contex={'category':category}
     return render(request, "product/category.html", contex)
+
+def collectionviews(request,slug):
+    if(Category.objects.filter(slug=slug, status=0)):
+        products=Product.objects.filter(category__slug=slug, status=0)
+        category=Category.objects.filter(slug=slug).first()
+
+        context={'products':products, 'category':category}
+    else:
+        messages.error(request, 'Category not found')
+        return redirect("/collections")
+    return render(request, "product/categoryview.html", context)
